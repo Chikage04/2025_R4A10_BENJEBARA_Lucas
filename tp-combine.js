19429d96e46c87e6738c63712fde426e773af2e3
chikage <lucas.benjebara9@gmail.com>
2025-02-03 23:57:18 +0100

    combine

--- tp-combine.js
+++ tp-combine.js
@@ -13,41 +13,50 @@ function getEmployees() {
 module.exports = {
   // 1) Renvoyer un nouveau tableau qui ne garde que les personnes dont le nom se termine par un "e"
   B1() {
-    // Implémentation ici
+    return getEmployees().filter(employe => employe.name.endsWith('e'))
   },
 
   // 2) Trouver la première personne dont l'âge est inférieur à 30
   B2() {
-    // Implémentation ici
+    return getEmployees().find(employe => parseInt(employe.age) < 30)
   },
 
   // 3) Renvoyer l'index où se trouve "Jeremy"
   B3() {
-    // Implémentation ici
+    return getEmployees().findIndex(employe => employe.name === "Jeremy")
   },
 
   // 4) Trier le tableau par âge des personnes
   B4() {
-    // Implémentation ici
+    return getEmployees().sort((a, b) => parseInt(a.age) - parseInt(b.age))
   },
 
   // 5) Ajouter Kasimu à la fin du tableau, il a 38 ans, et renvoyer le tableau modifié
   B5() {
-    // Implémentation ici
+    let employe = getEmployees()
+    
+    employe.push({ name: "Kasimu", age: "38" })
+    return employe
   },
 
   // 6) Renvoyer la longueur du tableau
   B6() {
-    // Implémentation ici
+    return getEmployees().length
   },
 
   // 7) Remplacer le "o" de Theo par un "a", et renvoyer le tableau modifié
   B7() {
-    // Implémentation ici
+    let employe = getEmployees();
+    let index = employe.findIndex(employe => employe.name === "Theo");
+
+      if (index !== -1) {
+        employe[theoIndex].name = employe[theoIndex].name.replace("o", "a");
+      }
+    return employe;
   },
 
   // 8) Renvoyer un tableau qui ne contient que les personnes ayant au moins un "e" dans leur nom
   B8() {
-    // Implémentation ici
+    return getEmployees().filter(employee => employee.name.includes("e"));
   },
 };
